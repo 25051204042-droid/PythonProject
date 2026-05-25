259f0cc0a413ecce583acc027f81c3bfa8b4d772
25051204042-droid <25051204042@mhs.unesa.ac.id>
2026-05-25 12:35:56 +0700

    wiiiii

--- UAS/ProjekUAS.kelompok.py
+++ UAS/ProjekUAS.kelompok.py
@@ -10,7 +10,7 @@ class Game:
     def __init__(self):
         pygame.init()
 
-        pygame.display.set_caption('AIS GEDRUK')
+        pygame.display.set_caption('WIIIIIIIIII')
         self.screen = pygame.display.set_mode((640, 480))
         self.display = pygame.Surface((320, 240))
 
